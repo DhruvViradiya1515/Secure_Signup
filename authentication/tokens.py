e59ea69dd11f9628b246525a2bd6c3eba652d3c2
DhruvViradiya1515 <76394347+DhruvViradiya1515@users.noreply.github.com>
2022-04-14 21:43:43 +0530

    Update tokens.py

--- authentication/tokens.py
+++ authentication/tokens.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.tokens import PasswordResetTokenGenerator
 from six import text_type
 
+# Token generation for creating unique link for email verification
+
 class TokenGenerator(PasswordResetTokenGenerator):
     def _make_hash_value(self, user, timestamp):
         return (text_type(user.pk) + text_type(timestamp))
