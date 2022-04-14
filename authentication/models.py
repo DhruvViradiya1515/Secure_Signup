27f60b1a1237e5f8350ca207c1461e5fa3a780e0
DhruvViradiya1515 <76394347+DhruvViradiya1515@users.noreply.github.com>
2022-04-14 21:44:46 +0530

    Update models.py

--- authentication/models.py
+++ authentication/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Contact(models.Model):
+    
+    # contact module for storing the feedback given by the user to save to database
     name = models.CharField(max_length=122)
     email = models.CharField(max_length=122)
     phone = models.CharField(max_length=12)
